ee5378f6113be5f80ddf04bd5bad9e8596ff7e22
akmalfitrianto <akmal2210010546@gmail.com>
2025-12-05 10:27:18 +0800

    minor fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -282,6 +282,7 @@
     </div>
 
     <!-- Alpine.js -->
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     {{--  Notification Actions Script --}}
